a4861e929e6c16f342f9c10e26305e5ee6c498d6
Anais Lee <lee.anais@gmail.com>
2016-04-09 16:48:22 -0400

    Added files via upload

--- App350/app/src/main/java/edu/upenn/cis350/entertainmenttracker/MainActivity.java
+++ App350/app/src/main/java/edu/upenn/cis350/entertainmenttracker/MainActivity.java
@@ -2,54 +2,31 @@ package edu.upenn.cis350.entertainmenttracker;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.graphics.Typeface;
+import android.widget.TextView;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.Scanner;
 
-import android.support.v7.app.AppCompatActivity;
+import edu.upenn.cis350.entertainmenttracker.datamanagement.RateSorter;
+import edu.upenn.cis350.entertainmenttracker.datamanagement.TitleSorter;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ViewPager mPager;
-    private PagerAdapter mPagerAdapter;
-    private ArrayList<Movie> movies;
-    private File savedMovies;
-
+    private static ArrayList<Movie> movies;
+    private String sortMethod = "RATING";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       //searchBar = (EditText) findViewById(R.id.searchBar);
-        // Instantiate a ViewPager and a PagerAdapter.
-        createExternalStoragePrivateFile();
-        movies = MovieRetriever.retrieveMovies(savedMovies);
-        mPager = (ViewPager) findViewById(R.id.pager);
-        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
-        mPager.setAdapter(mPagerAdapter);
-    }
+        movies = new ArrayList<Movie>();
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/centurygothic.TTF");
+        ((TextView) findViewById(R.id.header)).setTypeface(tf);
 
-    @Override
-    public void onBackPressed() {
-        if (mPager.getCurrentItem() == 0) {
-            // If the user is currently looking at the first step, allow the system to handle the
-            // Back button. This calls finish() on this activity and pops the back stack.
-            super.onBackPressed();
-        } else {
-            // Otherwise, select the previous step.
-            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
-        }
+        updateState();
     }
 
     @Override
@@ -74,80 +51,76 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void onSearchButtonClick(View v) {
-            Intent intent = new Intent(this, SearchActivity.class);
-            finish();
-            startActivity(intent);
+    public void updateState() {
+        switch (sortMethod) {
+            case "TITLE":
+                findViewById(R.id.buttonTitle).setBackgroundResource(R.drawable.ssfilled_left);
+                findViewById(R.id.buttonRating).setBackgroundResource(R.drawable.searchsort);
+                findViewById(R.id.buttonDate).setBackgroundResource(R.drawable.searchsort_right);
+                break;
+            case "RATING":
+                findViewById(R.id.buttonTitle).setBackgroundResource(R.drawable.searchsort_left);
+                findViewById(R.id.buttonRating).setBackgroundResource(R.drawable.ssfilled);
+                findViewById(R.id.buttonDate).setBackgroundResource(R.drawable.searchsort_right);
+                break;
+            case "DATE":
+                findViewById(R.id.buttonTitle).setBackgroundResource(R.drawable.searchsort_left);
+                findViewById(R.id.buttonRating).setBackgroundResource(R.drawable.searchsort);
+                findViewById(R.id.buttonDate).setBackgroundResource(R.drawable.ssfilled_right);
+                break;
+        }
+
+
     }
-/*
+
+
+
+    /**
+     * ON CLICK FUNCTIONS
+     * @param view
+     */
     public void onMovieClick(View view) {
 
         //create a new intent with this class and the new
         //activity class
-        Intent i = new Intent(this, MovieActivity.class);
+        Intent i = new Intent(this, AddMovie.class);
         startActivity(i);
     }
 
 
-    public void onSearchButtonClick(View v) {
-        searchTerm = searchBar.getText().toString().trim();
-
-        if (searchTerm.equals("")) {
-            Toast.makeText(MainActivity.this, "Please enter a movie name", Toast.LENGTH_SHORT).show();
-        } else {
-            Intent intent = new Intent(this, SearchActivity.class);
-            intent.putExtra("searchTerm", searchTerm);
-            finish();
-            startActivity(intent);
-        }
+    public void onSearchClick(View view) {
+        Intent intent = new Intent(this, SearchActivity.class);
+        overridePendingTransition(0, 0);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
     }
+
+    public void onWatchedClick(View view) {}
+
+    public void onToWatchClick(View view) {}
+
 
     //sort the movies based on the different data when certain button is clicked
 
-    public void onYearClick(View view) {
+//    public void onGenreClick(View view) {
+//        watchedMovies = GenreSorter.insertSort(watchedMovies);
+//    }
+
+    public void onDateClick(View view) {
+        //movies = YearSorter.insertSort(movies);
+        sortMethod = "DATE";
+        updateState();
     }
 
     public void onTitleClick(View view) {
         movies = TitleSorter.insertSort(movies);
+        sortMethod = "TITLE";
+        updateState();
     }
 
     public void onRateClick(View view) {
         movies = RateSorter.insertSort(movies);
-    } */
-
-    private void createExternalStoragePrivateFile() {
-        // Create a path where we will place our private file on external
-        // storage.
-        this.savedMovies = new File(getExternalFilesDir(null), "movies.txt");
-    }
-
-    private boolean hasExternalStoragePrivateFile() {
-        // Get path for the file on external storage.  If external
-        // storage is not currently mounted this will fail.
-        this.savedMovies = new File(getExternalFilesDir(null), "movies.txt");
-        if (savedMovies != null) {
-            return savedMovies.exists();
-        }
-        return false;
-    }
-
-    /**
-     * A simple pager adapter that represents movie objects, in
-     * sequence.
-     */
-    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
-        public ScreenSlidePagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return new ScreenSlidePageFragment();
-        }
-
-        @Override
-        public int getCount() {
-            return movies.size();
-        }
+        sortMethod = "RATING";
+        updateState();
     }
 }
